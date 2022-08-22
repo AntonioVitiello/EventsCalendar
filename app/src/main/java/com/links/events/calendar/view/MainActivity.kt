33ecc33ad68ea674d18f06c6af318883bd9b55d5
vitiello.antonio@gmail.com <p@ssw0rdD!>
2022-08-22 22:48:53 +0200

    initial commit

---
+++ app/src/main/java/com/links/events/calendar/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.links.events.calendar.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.links.events.calendar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/links/events/calendar/view/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.links.events.calendar.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.links.events.calendar.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
