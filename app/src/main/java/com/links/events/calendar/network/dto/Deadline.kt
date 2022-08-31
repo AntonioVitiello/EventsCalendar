76660c067093e0379b370e54059062801abec9b6
vitiello.antonio@gmail.com <p@ssw0rdD!>
2022-08-31 10:07:41 +0200

    added json format for DeadlineResponse

--- app/src/main/java/com/links/events/calendar/network/dto/Deadline.kt
+++ app/src/main/java/com/links/events/calendar/network/dto/Deadline.kt
@@ -1,8 +1,14 @@
 package com.links.events.calendar.network.dto
 
+import com.google.gson.annotations.SerializedName
+
 class Deadline(
+    @SerializedName("id")
     val id: String? = null,
+    @SerializedName("date")
     var date: String? = null,
+    @SerializedName("titles")
     val titles: List<String>? = null,
+    @SerializedName("desc")
     val desc: String? = null
 )
--- app/src/main/java/com/links/events/calendar/network/dto/DeadlineResponse.kt
+++ app/src/main/java/com/links/events/calendar/network/dto/DeadlineResponse.kt
@@ -1,5 +1,7 @@
 package com.links.events.calendar.network.dto
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Antonio Vitiello
  */
@@ -8,5 +10,6 @@ package com.links.events.calendar.network.dto
  * Params: date - date String in the format yyy/MM/dd  eg:"2022/09/25"
  */
 class DeadlineResponse(
+    @SerializedName("deadlines")
     val deadlines: List<Deadline>? = null
 )
